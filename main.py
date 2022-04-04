1e0fa3252ccd32e97bab00f0036d59a2b90373c3
zvpham <90424742+zvpham@users.noreply.github.com>
2022-04-04 21:52:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def load_words():
+  all_words = []
+  
+  with open('safedict_simple.txt', 'r') as f:
+    for line in f:
+      all_words.append(line.rstrip())
+  end_time = time.time()
+
+  elapsed_time = end_time - start_time
+  # log words loaded and elapsed time
+  print('Loaded ' + str(len(all_words)) + ' words in ' + f'{elapsed_time:.2f}' + ' seconds.')
+
+  return all_words
+
+with open('text.txt', 'w') as f:
+  
