929be329a2bfacf81aa936a38456f23c0cd558bb
zvpham <90424742+zvpham@users.noreply.github.com>
2022-04-06 15:27:27 +0000

    test_encoded Ceasar Cipher

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-file1 = open('test.txt', 'r')
-print(file1.read())
-file1.close()
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
 
-file1 =  open('test.txt', 'a')
-file1.write("\n Tomorrow")
-file1.close()
+# prerequisite - shift is an integer
+# shifts the alphabet by shift
+def encode_ceasar_cipher(message, shift):
+  
+  shifted_alphabet = [""] * 26
+  encoded_message = ""
+  
+  for i in range(26):
+    if i + shift >= 26:
+      shifted_alphabet[i + shift - 26] = alphabet[i]
+    else:
+      shifted_alphabet[i + shift] = alphabet[i]
+      
+  for letter in message:
+   encoded_message += shifted_alphabet(alphabet.getindexof("letter"))
 
-file1 = open('test.txt', 'r')
-print(file1.read())
-file1.close()
+shift = int(input("how much do you want to shift "))
+
+file1 = open('message.txt', 'r')
+message = file1.read()
+
+encode_ceasar_cipher(message, shift)
