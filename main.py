ff20f3f9b2fba8c2f368075ef622a01bb3474346
zvpham <90424742+zvpham@users.noreply.github.com>
2022-04-15 05:55:46 +0000

    added Decode Ceaser cipher

--- main.py
+++ main.py
@@ -8,17 +8,64 @@ def encode_ceasar_cipher(message, shift):
   encoded_message = ""
   
   for i in range(26):
-    if i + shift >= 26:
-      shifted_alphabet[i + shift - 26] = alphabet[i]
+    if i - shift <= -1:
+      shifted_alphabet[i - shift + 26] = alphabet[i]
     else:
-      shifted_alphabet[i + shift] = alphabet[i]
+      shifted_alphabet[i - shift] = alphabet[i]
       
   for letter in message:
-   encoded_message += shifted_alphabet(alphabet.getindexof("letter"))
+    if letter in alphabet:
+      encoded_message += shifted_alphabet[alphabet.index(letter)]
+    else:
+      encoded_message += letter
+  return encoded_message
 
+def decode_ceasar_cipher(message, shift):
+  shifted_alphabet = [""] * 26
+  decoded_message = ""
+  
+  for i in range(26):
+    if i - shift <= -1:
+      shifted_alphabet[i - shift + 26] = alphabet[i]
+    else:
+      shifted_alphabet[i - shift] = alphabet[i]
+      
+  for letter in message:
+    if letter in alphabet:
+      decoded_message += alphabet[shifted_alphabet.index(letter)]
+    else:
+      decoded_message += letter
+  print(decoded_message)
+
+def encode_vignere_cipher(message, key):
+  count = 0
+  encoded_message = ""
+  index = -1
+  
+  for letter in message:
+    if alphabet.index(letter) + alphabet.index(key[count]) > 25:
+        index = alphabet.index(letter) + alphabet.index(key[count]) - 25
+    else:
+        index = alphabet.index(letter) + alphabet.index(key[count])
+    encoded_message += alphabet[index]
+    if count >= len(key):
+      count = 0
+    else:
+      count += 1
+  return encoded_message
+       
 shift = int(input("how much do you want to shift "))
 
 file1 = open('message.txt', 'r')
-message = file1.read()
+message = file1.read().lower()
 
-encode_ceasar_cipher(message, shift)
+message.lower()
+"""
+cipher_text = encode_ceasar_cipher(message, shift)
+print("")
+print(cipher_text)
+print("")
+decode_ceasar_cipher(cipher_text, shift)
+"""
+print("test")
+print(encode_vignere_cipher(message, "key"))
